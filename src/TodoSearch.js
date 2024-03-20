5afc8a3603df35a736b38c305198ced05b2c52b6
JesusRamon2192 <jesus.ramon2192@gmail.com>
2024-03-19 20:46:59 -0600

    Clase 7 useState

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,14 +1,21 @@
+import React from "react";
 import "./TodoSearch.css";
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState(''); 
+
+    console.log('Los usuarios buscan todos de ' + searchValue);
+
     return (
         <input
             placeholder="Cortar cebolla"
             className="TodoSearch" 
+            value={searchValue}
             onChange={(event)=>{
-                console.log('Escribiste');
+                /* console.log('Escribiste');
                 console.log(event);
-                console.log(event.target.value);
+                console.log(event.target.value); */
+                setSearchValue(event.target.value);
             }}/>
     );
 }

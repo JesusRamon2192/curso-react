687676927cce6fe3de6ce9c1fcabda3d61ce080c
JesusRamon2192 <jesus.ramon2192@gmail.com>
2024-03-19 19:07:28 -0600

    Se crean componentes y se estilizan

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './platzi.webp';
-import './App.css';
+import {TodoCounter} from "./TodoCounter.js";
+import {TodoSearch} from "./TodoSearch.js";
+import {TodoList} from "./TodoList.js";
+import {TodoItem} from "./TodoItem.js";
+import {CreateTodoButton} from "./CreateTodoButton.js";
+import "./App.css";
+
+const defaultTodos = [
+    {text: 'Cortar Cebolla', completed: true},
+    {text: 'Tomar el curso de react', completed: false},
+    {text: 'Llorar con la llorona', completed: false},
+    {text: 'LALALALA', completed: false},
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <TodoCounter completed={16} total={25} />
+      <TodoSearch />
+
+      <TodoList>
+        {defaultTodos.map(todo => (
+            <TodoItem 
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}/>
+        ))}
+      </TodoList>
+
+        <CreateTodoButton/>
+    </>
   );
 }
 
